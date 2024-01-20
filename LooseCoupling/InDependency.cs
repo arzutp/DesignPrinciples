ae070af23bddeeba97ddbb355140a00ec72f1086
arzutepe <38106558+arzutp@users.noreply.github.com>
2024-01-20 22:50:20 +0300

    Add project files.

---
+++ LooseCoupling/InDependency.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LooseCoupling.InDependecy;
+
+class MailSender
+{
+    public void Send(IMailServer mailServer)
+    {
+        mailServer.Send("aaaa@fff.com", "lllll");
+    }
+}
+
+interface IMailServer
+{
+    void Send(string to, string body);
+}
+
+class Gmail : IMailServer
+{
+    public void Send(string to, string body)
+    {
+        Console.WriteLine("Gmail");
+    }
+}
+
+class HotMail : IMailServer
+{
+    public void Send(string to, string body)
+    {
+        Console.WriteLine("Hotmail");
+    }
+}
+
+class Yandex : IMailServer
+{
+    public void Send(string to, string body)
+    {
+        Console.WriteLine("Yandex");
+    }
+}
---
+++ LooseCoupling/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+#region bağımlı örnek
+//class MailSender{
+//    public void Send()
+//    {
+//        Gmail gmail = new();
+//        gmail.Send();
+//    }
+//}
+
+//class Gmail
+//{
+//    public void Send() { 
+//        ///
+//    }
+//}
+#endregion
+
+using LooseCoupling.InDependecy;
+
+MailSender sender = new();
+sender.Send(new Gmail());
+sender.Send(new HotMail());
+
+
+//burda yazılımcı karar vermemiş oluyoruz. Böyle kullanılmasın doğru bir örnek değildirç
+//MailSender sender2 = new();
+//sender2.Send();
+//sender2.Send();
+
+
+
+
+
+
+
+
+
+
+
