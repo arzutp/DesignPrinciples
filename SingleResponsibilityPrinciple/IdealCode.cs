682f78f3133a4d86df5098f89917ef797b60ec3e
arzutepe <38106558+arzutp@users.noreply.github.com>
2024-01-21 12:53:15 +0300

    single responsibility principle

---
+++ SingleResponsibilityPrinciple/IdealCode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple.IdealCode;
+
+class Database
+{
+    public void Connect()
+    {
+        //...
+        Console.WriteLine("Veritabanı bağlantısı sağlanmıştır.");
+    }
+    public void Disconnect()
+    {
+        //...
+        Console.WriteLine("Veritabanı bağlantısı kesilmiştir.");
+    }
+    public string State { get; set; }
+}
+
+class PersonService
+{
+    public List<Person> GetPersons()
+    {
+        return new() {
+            new(){ Name = "Aslı", Surname = "Sütçüoğlu" },
+            new(){ Name = "Kubilay", Surname = "Peynircioğlu" },
+            new(){ Name = "Tahsin", Surname = "Sütçüoğlu" }
+        };
+    }
+}
---
+++ SingleResponsibilityPrinciple/NotIdealCode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple.NotIdealCode
+{
+    public class Database
+    {
+        public void Connect()
+        {
+            Console.WriteLine("Veritabanı bağlantısı sağlanmıştır.");
+        }
+        public void Disconnect() { 
+            Console.WriteLine("Veritabanı bağlantısı kesilmiştir.");
+        }
+
+        public string Stage { get; set; } = null!;
+
+        public List<Person> GetPersons()
+        {
+            return new() {
+            new(){ Name = "Aslı", Surname = "Sütçüoğlu" },
+            new(){ Name = "Kubilay", Surname = "Peynircioğlu" },
+            new(){ Name = "Tahsin", Surname = "Sütçüoğlu" }
+        };
+        }
+    }
+}
