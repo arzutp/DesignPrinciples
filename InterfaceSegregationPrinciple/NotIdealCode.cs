5f170a0dd4d9426ba666d29ea1a57a7d5d17cb9e
arzutepe <38106558+arzutp@users.noreply.github.com>
2024-01-24 14:54:43 +0300

    interface-segregation-principle

---
+++ InterfaceSegregationPrinciple/NotIdealCode.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregationPrinciple.NotIdealCode;
+
+interface IPrinter
+{
+    void Print();
+    void Scan();
+    void Fax();
+    void PrintDuplex();
+
+}
+
+class HPPrinter : IPrinter
+{
+    public void Fax()
+    {
+        //fax 
+    }
+
+    public void Print()
+    {
+        //print
+    }
+
+    public void PrintDuplex()
+    {
+        //print duplex
+    }
+
+    public void Scan()
+    {
+        //scan
+    }
+}
+
+class SamsungPrinter : IPrinter
+{
+    public void Fax()
+    {
+        //... Fax işlemleri ...
+    }
+
+    public void Print()
+    {
+        //... Print işlemleri ...
+    }
+
+    public void PrintDuplex()
+        => throw new NotImplementedException();
+
+
+    public void Scan()
+        => throw new NotImplementedException();
+}
+
+class LexmarkPrinter : IPrinter
+{
+    public void Fax()
+    {
+        //... Fax işlemleri ...
+    }
+
+    public void Print()
+    {
+        //... Print işlemleri ...
+    }
+
+    public void PrintDuplex()
+        => throw new NotSupportedException();
+
+    public void Scan()
+    {
+        //... Scan işlemleri ...
+    }
+}
---
+++ InterfaceSegregationPrinciple/Program.cs
@@ -0,0 +1,11 @@
+﻿#region Not Ideal Code
+
+//using InterfaceSegregationPrinciple.NotIdealCode;
+
+//SamsungPrinter printer = new();
+//printer.PrintDuplex();  //burada bu şekilde kullanmak istediğimizde hata alırız.
+#endregion
+
+#region IdealCode
+
+#endregion
